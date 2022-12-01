c465c8b15f9aad3fc77e09c00370a24ad0b053db
MirpoCasas <mirpocasas@gmail.com>
2022-12-01 02:24:42 -0300

    checkpoint

--- Front/scripts.js
+++ Front/scripts.js
@@ -1,5 +1,14 @@
+const authKey = localStorage.getItem("auth");
+console.log(authKey);
+if (authKey === null) {
+    // window.location.href = "../Login/index.html";
+}
+
+let fetchPage = 1;
 /* API key = 47521ec4cb0fc520db13de6730790654 */
-let movies = []
+let movies = [];
+let moviesToAdd = [];
+let layout = "vertical"
 
 let genres = [
     {
@@ -83,32 +92,32 @@ let genres = [
 //gets img based on source and desired size
 function getImg(source, size) {
     if (source === null) {
-        return ""
+        return "";
     }
-    const IMG_URL = 'https://image.tmdb.org/t/p/';
+    const IMG_URL = "https://image.tmdb.org/t/p/";
     let final = IMG_URL + size + source;
     return final;
 }
 
 //creates a card for the display
-function createCard(title, desc, position, img_path) {
-    console.log(`creating card ${position}`)
-    desc = abridge(desc)
-    const cardPlace = 'card' + position;
-    const newCard = document.createElement('div');
-    let imgDone = getImg(img_path, "original")
-    newCard.style.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%), url(${imgDone})`
-    newCard.classList.add(cardPlace, 'vertical');
+function createCard(lay, title, desc, position, img_path) {
+    desc = abridge(desc);
+    const cardPlace = "card" + position;
+    const newCard = document.createElement("div");
+    let imgDone = getImg(img_path, "original");
+    newCard.style.backgroundImage = `url(${imgDone})`;
+    newCard.classList.add(cardPlace, lay);
     newCard.innerHTML = `
+    <div class="cardgradient">
     <h2 id="_title">${title}</h2>
     <p id="${cardPlace}_desc">${desc}</span>
-    `
+    </div>
+    `;
 
     return newCard;
 }
 //returns genres from ids
 function giveGenres(genrelist) {
-    console.log('seting genres');
     res = [];
 
     genrelist.forEach((genre_id) => {
@@ -124,51 +133,47 @@ function giveGenres(genrelist) {
 //shroten msg for card
 function abridge(message) {
     if (message.length > 84) {
-      return message.slice(0,140) + " ...";
+        return message.slice(0, 140) + " ...";
     } else {
-      return message
+        return message;
     }
 }
-  
+
 //fetch to get movie data. returns JSON.
-const getMoviesData = async () => {
+async function getMoviesData(pageNum) {
     const requestOptions = {
         method: "GET",
         redirect: "follow",
     };
-    
+
     try {
         const res = await fetch(
-            "https://api.themoviedb.org/3/movie/popular?api_key=47521ec4cb0fc520db13de6730790654&language=en-US&page=1/",
+            `https://api.themoviedb.org/3/movie/popular?api_key=47521ec4cb0fc520db13de6730790654&language=en-US&page=${pageNum}/`,
             requestOptions
         );
         const data = await res.json();
 
-        return data
-        
+        return data;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-};
+}
 // assign movies to variable
-const setMovies = async () => {
-    console.log('asingnig value to movies');
-    const raw = await getMoviesData();
-    
+async function setMovies() {
+    const raw = await getMoviesData(fetchPage);
+
     movies = await raw.results;
-    console.log('data assigned');
+
     return movies;
 }
 
 //populates the title card
-function setMain() {
-    console.log('setting main movie');
-    let mainMovie = movies[0];
+function setMain(mainMovie) {
     let title = mainMovie.original_title;
     let desc = mainMovie.overview;
-    let genres = giveGenres(mainMovie.genre_ids).join(', ');
-    const cont = document.getElementById('frontcont');
-    let newEl = document.createElement('div');
+    let genres = giveGenres(mainMovie.genre_ids).join(", ");
+    const cont = document.getElementById("frontcont");
+    let newEl = document.createElement("div");
     newEl.innerHTML = `
     <p class="genre" id="preview_genre">${genres}</p>
     <h2 class="movietitle" id="preview_title">${title}</h2>
@@ -179,78 +184,108 @@ function setMain() {
     </div>
     `;
     cont.append(newEl);
-    
-    let background = document.getElementById('frontpreview')
-    let img = getImg(mainMovie.backdrop_path, "original")
-    background.style.backgroundImage = `linear-gradient(90deg, #080E20 0%, rgba(29, 29, 29, 0) 65.46%), URL(${img})`
+
+    let background = document.getElementById("frontpreview");
+    let img = getImg(mainMovie.backdrop_path, "original");
+    background.style.backgroundImage = `linear-gradient(90deg, #080E20 0%, rgba(29, 29, 29, 0) 65.46%), URL(${img})`;
+}
+
+function createList(movieArr) {
+    const recom = document.getElementById("recom");
+
+    let leftover = movieArr.length % 3;
+    moviesToAdd.push(movieArr.splice(-leftover));
+
+    for (i = 0; i < movieArr.length; i += 3) {
+        let pos = 1;
+        let plusGrid = document.createElement("div");
+        plusGrid.classList.add("cards", layout);
+
+        let fakei = i;
+
+        for (j = 1; j < 4; j += 1) {
+            if (movieArr[fakei] != undefined) {
+                // eval("let card" + j +";")
+                eval(
+                    "card" +
+                        j +
+                        " = createCard(layout,movieArr[fakei].original_title, movieArr[fakei].overview, pos, movieArr[fakei].backdrop_path);"
+                );
+                eval("plusGrid.append(card" + j + ");");
+            }
+            pos += 1;
+            fakei += 1;
+        }
+        recom.append(plusGrid);
+    }
 }
 
 //creates card array
-const createList = async () => {
-    console.log('starting create list');
-    movies = await setMovies();
-    console.log('data in create list');
-    const recom = document.getElementById("recom")
+async function createPage() {
+    
+    await setMovies();
+
+    const recom = document.getElementById("recom");
     const menu3 = document.getElementById("menu3");
     const menu1 = document.getElementById("menu1");
+
+    const movieToMain = movies.shift();
+    setMain(movieToMain);
     
-    console.log(movies);
-    console.log(movies.length)
-    setMain();
 
-    let count = 0
-
-    for (i = 1; i < movies.length; i += 3){
-        let pos = 1
-        let plusGrid = document.createElement("div");
-        plusGrid.classList.add("cards", "vertical");
-        
-        let fakei = i
-
-        console.log("i value " + i)
-
-        for (j = 1; j < 4; j += 1){
-            console.log("j value " + j)
-  
-            console.log("fakei value " + fakei)
-            
-            if (movies[fakei] != undefined){
-                // eval("let card" + j +";")
-                eval("card" + j +" = createCard(movies[fakei].original_title, movies[fakei].overview, pos, movies[fakei].backdrop_path);")
-                eval("console.log(card" + j +");") 
-                eval("plusGrid.append(card" + j +");") 
-            } else {
-            
-                console.log("skipping");
-            }
-              pos += 1;
-              fakei += 1;   
-          }
-
-    
-        console.log(plusGrid)
-        recom.append(plusGrid)
-        console.log(count)
-        count++
-        console.log(count)
-    }
-    console.log(count)
+    createList(movies)
 
     menu1.addEventListener("click", function () {
+        layout = "horizontal"
+        let cards = document.querySelectorAll(".card1,.card2,.card3")
         
+        cards.forEach(card => {
+            card.classList.remove("vertical")    
+            card.classList.add("horizontal")    
+        });
+
+        let conts = document.querySelectorAll(".cards")
+        conts.forEach(array => {
+            array.classList.remove("vertical")
+            array.classList.add("horizontal")
+        });
+        let loader = document.querySelector(".loader");
+        let loaderItems = document.querySelectorAll(".loaderItem");
+        loader.classList.remove("vertical");
+        loader.classList.add("horizontal");
+        
+        loaderItems.forEach(item => {
+            item.classList.remove("vertical")
+            item.classList.add("horizontal")
+        });
     });
     menu3.addEventListener("click", function () {
-       
+        layout = "vertical"
+        let cards = document.querySelectorAll(".card1,.card2,.card3")
+        cards.forEach(card => {
+            card.classList.remove("horizontal")    
+            card.classList.add("vertical")    
+        });
+        
+        let conts = document.querySelectorAll(".cards")
+        conts.forEach(array => {
+            array.classList.remove("horizontal")
+            array.classList.add("vertical")
+        });
+
+        let loader = document.querySelector(".loader");
+        let loaderItems = document.querySelectorAll(".loaderItem");
+        loader.classList.remove("horizontal");
+        loader.classList.add("vertical");
+
+        loaderItems.forEach(item => {
+            item.classList.remove("horizontal")
+            item.classList.add("vertical")
+        });
     });
-    ;
 }
 
 window.onload = () => {
-    const authKey = localStorage.getItem('auth')
-    console.log(authKey)
-    if (authKey === null) {
-        window.location.href = '../Login/index.html'
-    }
-    createList()
-}
-
+    createPage();
+    console.log(moviesToAdd);
+};
